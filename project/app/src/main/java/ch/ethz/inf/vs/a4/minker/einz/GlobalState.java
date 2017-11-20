8514ed5cf2b44516c90f5c82291dd6dab5f21006
Sansero <simcity@live.de>
2017-11-20 14:09:08 +0100

    Added Global state. WIP

---
+++ project/app/src/main/java/ch/ethz/inf/vs/a4/minker/einz/GlobalState.java
@@ -0,0 +1,82 @@
+package ch.ethz.inf.vs.a4.minker.einz;
+
+import org.json.JSONObject;
+
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Stack;
+
+
+/**
+ * Created by Josua on 11/19/17.
+ */
+
+public class GlobalState {
+    private Stack<Card> discardPile;
+    private Queue<Card> drawPile;
+    private Player activePlayer;
+    private List<Player> players;
+
+    public GlobalState(){
+        this(new LinkedList<Card>());
+    }
+
+    public GlobalState(Collection<Card> drawPile) {
+        this(drawPile, new Stack<Card>());
+    }
+
+    private GlobalState(Collection<Card> drawPile, Stack<Card> discardPile) {
+        this.discardPile = discardPile;
+        this.drawPile = new LinkedList<>(drawPile);
+
+    }
+
+    public List<Card> getDiscardPile() {
+        return discardPile.subList(0, discardPile.size() - 1);
+    }
+
+    public void playCatd(Card card){
+        discardPile.add(card);
+    }
+
+    /**
+     * @return The Top card of the draw pile or null if the draw pile is empty
+     */
+    public Card drawCard() {
+        if (drawPile.isEmpty()) {
+            return null;
+        }
+        return drawPile.poll();
+    }
+
+    public int drawPileSize() {
+        return drawPile.size();
+    }
+
+    /**
+     * Adds cards at the bottom of the draw pile.
+     * @param cards Collection of cards to add to the bottom of the draw pile
+     */
+    public void addCardstoDrawPile(Collection<Card> cards){
+        drawPile.addAll(cards);
+    }
+
+    public Player getActivePlayer() {
+        return activePlayer;
+    }
+
+    public void setActivePlayer(Player player) {
+        activePlayer = player;
+    }
+
+    public JSONObject toJSON() {
+        return null;
+    }
+
+    public static GlobalState fromJSON(JSONObject jsonObject) {
+        return null;
+    }
+}
