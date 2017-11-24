5714a85fad7d34dc0236f38d9b8fb65391e0e1e9
sisilvia <sisilvia@student.ethz.ch>
2017-11-24 14:54:14 +0100

    the missing parser

---
+++ project/app/src/main/java/ch/ethz/inf/vs/a4/minker/einz/messageparsing/parsertypes/EinzFurtherActionsParser.java
@@ -0,0 +1,10 @@
+package ch.ethz.inf.vs.a4.minker.einz.messageparsing.parsertypes;
+
+import ch.ethz.inf.vs.a4.minker.einz.messageparsing.EinzParser;
+
+/**
+ * Created by silvia on 11/24/17.
+ */
+
+public class EinzFurtherActionsParser extends EinzParser {
+}
