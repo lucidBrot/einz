a5ab41579d2041fcdc2253b292752b77d814d03d
Eric Mink <eric@mink.li>
2017-11-28 15:32:36 +0100

    client now handles RegistrationFailure as well.

--- project/app/src/main/java/ch/ethz/inf/vs/a4/minker/einz/messageparsing/actiontypes/EinzRegisterFailureAction.java
+++ project/app/src/main/java/ch/ethz/inf/vs/a4/minker/einz/messageparsing/actiontypes/EinzRegisterFailureAction.java
@@ -3,6 +3,7 @@ package ch.ethz.inf.vs.a4.minker.einz.messageparsing.actiontypes;
 import ch.ethz.inf.vs.a4.minker.einz.client.ClientActionCallbackInterface;
 import ch.ethz.inf.vs.a4.minker.einz.messageparsing.EinzAction;
 import ch.ethz.inf.vs.a4.minker.einz.messageparsing.EinzMessage;
+import ch.ethz.inf.vs.a4.minker.einz.messageparsing.messagetypes.EinzRegisterFailureMessageBody;
 import ch.ethz.inf.vs.a4.minker.einz.server.EinzServerClientHandler;
 import ch.ethz.inf.vs.a4.minker.einz.server.EinzServerManager;
 import ch.ethz.inf.vs.a4.minker.einz.gamelogic.ServerFunctionDefinition;
@@ -31,7 +32,8 @@ public class EinzRegisterFailureAction extends EinzAction {
     /**
      * compatibility for new actionfactory (for client)
      */
-    public EinzRegisterFailureAction(ServerFunctionDefinition sInterface, EinzServerManager serverManager, EinzMessage params, String issuedByPlayer, EinzServerClientHandler issuedByClientHandler, ClientActionCallbackInterface clientActionCallbackInterface, Object completelyCustom){
+    public EinzRegisterFailureAction(ServerFunctionDefinition sInterface, EinzServerManager serverManager, EinzMessage<EinzRegisterFailureMessageBody> params, String issuedByPlayer, EinzServerClientHandler issuedByClientHandler, ClientActionCallbackInterface clientActionCallbackInterface, Object completelyCustom){
         super(sInterface, serverManager, params, issuedByPlayer, issuedByClientHandler, clientActionCallbackInterface, completelyCustom);
+        getClientActionCallbackInterface().onRegisterFailure(params);
     }
 }
