33f1da50f85650bef42f5e6b133e455281e9cfb6
Eric Mink <eric@mink.li>
2017-11-30 19:37:54 +0100

    but UTF-32 is cooler than UTF-8

--- project/app/src/main/java/ch/ethz/inf/vs/a4/minker/einz/Globals.java
+++ project/app/src/main/java/ch/ethz/inf/vs/a4/minker/einz/Globals.java
@@ -1,5 +1,7 @@
 package ch.ethz.inf.vs.a4.minker.einz;
 
 public class Globals {
-    public static final String ENCODING = "UTF-8"; // the encoding for sending/receiving strings
+    public static final String ENCODING = "UTF-32"; // the encoding for sending/receiving strings
+    // UTF-8 is the probable default of the JVM
+    // UTF-32 could work too though
 }
