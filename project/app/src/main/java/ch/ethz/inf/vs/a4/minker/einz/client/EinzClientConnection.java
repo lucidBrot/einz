4adc394f3b1f036711500778175d163164af8526
Eric Mink <eric@mink.li>
2017-12-02 15:51:30 +0100

    fix applied

--- project/app/src/main/java/ch/ethz/inf/vs/a4/minker/einz/client/EinzClientConnection.java
+++ project/app/src/main/java/ch/ethz/inf/vs/a4/minker/einz/client/EinzClientConnection.java
@@ -190,11 +190,11 @@ public class EinzClientConnection implements Runnable {
      * Note: Closing a socket doesn't clear its connection state, which means this method will return true for a closed socket (see isClosed()) if it was successfuly connected prior to being closed.
      */
     public boolean isConnected() {
-        //return (this.socket != null && this.socket.isConnected());
+        return (this.socket != null && this.socket.isConnected());
 
 
         // old version. bad.
-        return (this.socket == null || this.socket.isConnected());
+        //return (this.socket == null || this.socket.isConnected());
     }
 
     public interface OnMessageReceived {
