9a66127dda04fb13898c5fccd5f0762ac9dc4eb8
Christian Knieling <cknieling@outlook.com>
2017-12-03 23:07:22 +0100

    added comment for testing purposes

--- project/app/src/main/java/ch/ethz/inf/vs/a4/minker/einz/UI/ClientSetupActivity.java
+++ project/app/src/main/java/ch/ethz/inf/vs/a4/minker/einz/UI/ClientSetupActivity.java
@@ -31,7 +31,7 @@ public class ClientSetupActivity extends AppCompatActivity implements View.OnCli
                         | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                         | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
                         | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
-                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY); // make sticky
     }
 
     @Override
