e0fa37d4cefbba8fbc351b20a28cffc76f2678db
Eric Mink <eric@mink.li>
2017-12-13 21:33:09 +0100

    added fix for when card origin is null

--- project/app/src/main/java/ch/ethz/inf/vs/a4/minker/einz/model/cards/Card.java
+++ project/app/src/main/java/ch/ethz/inf/vs/a4/minker/einz/model/cards/Card.java
@@ -110,6 +110,9 @@ public class Card {
         JSONObject card = new JSONObject();
         try {
             card.put("ID", this.ID);
+            if(this.origin==null){
+                this.origin=CardOrigin.UNSPECIFIED.value;
+            }
             card.put("origin", this.origin);
         } catch (JSONException e) {
             e.printStackTrace(); // this will not happen. EVER.
