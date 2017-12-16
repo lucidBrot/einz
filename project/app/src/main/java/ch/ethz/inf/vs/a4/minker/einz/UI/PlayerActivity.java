4a4c4a4eb7df3cf612f38dea1c807eedd6292059
Sansero <simcity@live.de>
2017-12-16 15:03:12 +0100

    Fixed Problem with the backstack after the game finishes

--- project/app/src/main/java/ch/ethz/inf/vs/a4/minker/einz/UI/PlayerActivity.java
+++ project/app/src/main/java/ch/ethz/inf/vs/a4/minker/einz/UI/PlayerActivity.java
@@ -814,7 +814,9 @@ public class PlayerActivity extends FullscreenActivity implements GameUIInterfac
 
     public void goBackToMainMenu(){
         Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK );
         startActivity(intent);
+        finish();
     }
 
     @Override
